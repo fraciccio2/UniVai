dcdd2bdb8e86094ba651cfef68704c880bc04ae4
Francesco Giacomazzo <GCMFNC01S18B428X@studium.unict.it>
2023-04-01 23:40:31 +0200

    feat: created login and signup class with sqlite

--- CarSharingProject/app/src/main/java/com/example/carsharing/LoginActivity.java
+++ CarSharingProject/app/src/main/java/com/example/carsharing/LoginActivity.java
@@ -2,13 +2,54 @@ package com.example.carsharing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.carsharing.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    HelperDataBase helperDataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        helperDataBase = new HelperDataBase(this);
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.loginEmail.getText().toString();
+                String password = binding.loginPassword.getText().toString();
+
+                if(email.equals("") || password.equals("")){
+                    Toast.makeText(LoginActivity.this, "Tutti i campi sono obbligatori", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkCredentials = helperDataBase.checkEmailAndPassword(email, password);
+
+                    if(checkCredentials) {
+                        Toast.makeText(LoginActivity.this, "Login effettuata con successo", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Email o password errati", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        binding.signUpRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- CarSharingProject/app/src/main/java/com/example/carsharing/SignupActivity.java
+++ CarSharingProject/app/src/main/java/com/example/carsharing/SignupActivity.java
@@ -2,13 +2,63 @@ package com.example.carsharing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.carsharing.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
+
+    ActivitySignupBinding binding;
+    HelperDataBase helperDataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        helperDataBase = new HelperDataBase(this);
+
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.signupEmail.getText().toString();
+                String password = binding.signupPassword.getText().toString();
+                String confirmPassword = binding.signupConfirmPassword.getText().toString();
+
+                if(email.equals("") || password.equals("") || confirmPassword.equals("")){
+                    Toast.makeText(SignupActivity.this, "Tutti i campi sono obbligatori", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(password.equals(confirmPassword)) {
+                        Boolean checkUserEmail = helperDataBase.checkEmail(email);
+                        if(!checkUserEmail) {
+                            Boolean insert = helperDataBase.insertData(email, password);
+                            if(insert) {
+                                Toast.makeText(SignupActivity.this, "Registrazione avvenuta con successo", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(SignupActivity.this, "Errore nella registrazione", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(SignupActivity.this, "Email già presente", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(SignupActivity.this, "Le password inserite sono diverse", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        binding.loginRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
