3dd2cdb05504f961eb7fe2bdb00fc69e170fdf16
giacomazzof <francesco.giacomazzo@rbtech.dev>
2023-04-06 11:41:22 +0200

    feat: added autocomplete for insert address

--- CarSharingProject/app/src/main/java/com/example/carsharing/activities/AddInfoActivity.java
+++ CarSharingProject/app/src/main/java/com/example/carsharing/activities/AddInfoActivity.java
@@ -1,9 +1,11 @@
 package com.example.carsharing.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
@@ -11,17 +13,29 @@ import android.widget.Toast;
 import com.example.carsharing.R;
 import com.example.carsharing.databinding.ActivityAddInfoBinding;
 import com.example.carsharing.models.UserModel;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Arrays;
+
 public class AddInfoActivity extends AppCompatActivity {
 
     ActivityAddInfoBinding binding;
-    String[] items = {"ciao"};
+    String[] items = {
+            "Giurisprudenza", "Economia e Impresa", "Scienze Politiche e Sociali", "Scienze Umanitarie",
+            "Scienze della Formazione", "Scuola di Medicina", "Scienze del Farmaco e della Formazione", "Agricoltura, Alimentazione e Ambiente, Igneria Civile e Architettura",
+            "Igneria Elettrica, Elettronica e Informatica", "Fisica e Astronomia", "Matematica e Informatica",
+            "Scienza Biologiche, Geologiche e Ambientali", "Scienze Chimiche", "Scuola Superiore di Catania", "Scuola di Lingua e Cultura Italiana per Stranieri"
+    };
     ArrayAdapter<String> adapterItems;
     FirebaseAuth mAuth;
     DatabaseReference mDatabase;
+    String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +47,33 @@ public class AddInfoActivity extends AppCompatActivity {
 
         adapterItems = new ArrayAdapter<String>(this, R.layout.list_item, items);
         binding.autoCompleteText.setAdapter(adapterItems);
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.start_address);
+        if (autocompleteFragment != null) {
+            autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+            autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onError(@NonNull Status status) {
+                    Log.i("ERROR", "An error occurred: " + status);
+                }
+
+                @Override
+                public void onPlaceSelected(@NonNull Place place) {
+                    address = place.getAddress();
+                }
+            });
+        }
 
         binding.startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String name = binding.startName.getText().toString();
                 String surname = binding.startSurname.getText().toString();
-                String address = binding.startAddress.getText().toString();
-                String university = binding.startSelectUni.getTransitionName();
+                String university = binding.autoCompleteText.getText().toString();
                 Boolean hasCar = binding.startCar.isChecked();
-                if(name.equals("") || surname.equals("")){
+                if (name.equals("") || surname.equals("") || university.equals("") || address.equals("")) {
                     Toast.makeText(AddInfoActivity.this, "Inserisci tutti i campi necessari", Toast.LENGTH_SHORT).show();
                 } else {
-                    if(mAuth.getCurrentUser() != null) {
+                    if (mAuth.getCurrentUser() != null) {
                         UserModel user = new UserModel(name, surname, address, university, hasCar);
                         mDatabase.child("users").child(mAuth.getCurrentUser().getUid()).setValue(user);
                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
--- CarSharingProject/app/src/main/java/com/example/carsharing/activities/MainActivity.java
+++ CarSharingProject/app/src/main/java/com/example/carsharing/activities/MainActivity.java
@@ -1,26 +1,39 @@
 package com.example.carsharing.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.carsharing.R;
 import com.example.carsharing.databinding.ActivityMainBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int PERMISSIONS_REQUEST_LOCATION = 123;
     ActivityMainBinding binding;
     GoogleMap gMap;
-    FrameLayout map;
+    FusedLocationProviderClient providerClient;
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +41,54 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
         binding.bottomNavigationView.setBackground(null);
+        providerClient = LocationServices.getFusedLocationProviderClient(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.main_map);
         mapFragment.getMapAsync(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST_LOCATION);
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.gMap = googleMap;
+        gMap = googleMap;
+        this.loadMap();
+    }
 
-        LatLng mapItaly = new LatLng(41.87194, 12.56738);
-        this.gMap.addMarker(new MarkerOptions().position(mapItaly));
-        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapItaly));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                this.loadMap();
+            }
+        }
+    }
+
+    private void loadMap() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            providerClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        LatLng mapItaly = new LatLng(location.getLatitude(), location.getLongitude());
+                        gMap.addMarker(new MarkerOptions().position(mapItaly));
+                        gMap.moveCamera(CameraUpdateFactory.newLatLng(mapItaly));
+                    }
+                }
+            });
+
+            providerClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("Error", "exception", e);
+                }
+            });
+        }
     }
 }
